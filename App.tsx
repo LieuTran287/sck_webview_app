d277fef3cd9afc97eacae046bc739a461ad204fb
chaunnt <chaupad@gmail.com>
2023-03-05 13:41:37 +0700

    cp nhat link sang mcredit.vaytien-nhanh247.

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 import WebView from "react-native-webview";
 function App(): JSX.Element {
-  const uriDefault = 'https://vaynhanh-mcredit.com/'
+  const uriDefault = 'https://mcredit.vaytien-nhanh247.com/'
   const [uri, setUri] = useState(uriDefault)
   const webviewRef = useRef<any>();
 
